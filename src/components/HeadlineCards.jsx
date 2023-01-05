d1d930006d8dc7c0d961c633c80cb3ab3b7d77a9
Stiven Fortes <stiven.fortes@devgo.org>
2023-01-05 12:01:18 -0100

    Built a responsive layout

---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function HeadlineCards() {
+  return (
+    <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+      {/*Card */}
+      <div className="rounded-xl relative">
+        {/*Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Sun's out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="/"
+        />
+      </div>
+      {/*Card */}
+      <div className="rounded-xl relative">
+        {/*Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">New Restaurants</p>
+          <p className="px-2">Added Daily</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/33162/food-restaurant-menu-asia.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="/"
+        />
+      </div>
+      {/*Card */}
+      <div className="rounded-xl relative">
+        {/*Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">We Deliver Desserts</p>
+          <p className="px-2">Tasty Treats</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/5639261/pexels-photo-5639261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="/"
+        />
+      </div>
+    </div>
+  );
+}
